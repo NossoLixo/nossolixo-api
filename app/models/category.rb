300968e52d784f3012e6b0e445821e2978d1e89b
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-23 11:08:15 -0300

    Specify category json columns

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,10 @@ class Category < ApplicationRecord
 
   scope :approved, -> { where(approved: true) }
 
+  def as_json(options = {})
+    super(options.merge(only: [:id, :name, :color]))
+  end
+
   def approve!
     update(approved: true, approved_at: Time.current)
   end
