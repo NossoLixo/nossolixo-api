a56dc952009a4d8ed690216bccfe5b2ae8c1c370
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-25 23:34:14 -0300

    Add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  enum role: [:guest, :admin]
 end
