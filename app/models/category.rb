8562a08fbdd7708eefdd907b3f9f3c9699840ecf
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-30 10:33:23 -0300

    Define category as json category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,18 @@
 class Category < ApplicationRecord
   include Approvable
 
+  REJECTED_ATTRIBUTES = [
+    :requested_by_id,
+    :approved_by_id,
+    :approved_at,
+    :created_at,
+    :updated_at
+  ].freeze
+
   validates :name, :color, presence: true
   validates :color, uniqueness: { case_sensitive: false }
+
+  def as_json(options = {})
+    super(options.merge(except: REJECTED_ATTRIBUTES))
+  end
 end
