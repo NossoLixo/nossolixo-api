f23a104ceefac16c42519414633e3710b3b8d52d
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-30 10:47:54 -0300

    Adds city and categories in places json

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,17 @@ class Place < ApplicationRecord
   include Approvable
 
   validates :name, :city, :street, :lat, :lng, presence: true
+  has_and_belongs_to_many :categories
 
   belongs_to :city
+
+  def as_json(options = {})
+    place = {
+      except: [:city_id, :requested_by_id, :approved_by_id, :approved_at, :created_at, :updated_at]
+    }
+    city = { only: [:id, :name, :state] }
+    categories = { only: [:id, :name, :color] }
+    associations = { include: { city: city, categories: categories } }
+    super(options.merge(place.merge(associations)))
+  end
 end
