f0f203a900cc45d716a593e4df9fb054aba27566
Thiago Guimaraes <thiagogsr@gmail.com>
2016-11-19 16:31:34 -0300

    Remove unncessary method

--- app/models/concerns/insensitive_order.rb
+++ app/models/concerns/insensitive_order.rb
@@ -2,12 +2,8 @@ module InsensitiveOrder
   extend ActiveSupport::Concern
 
   class_methods do
-    def build_criteria(attribute)
-      "TRIM(LOWER(UNACCENT(#{attribute})))"
-    end
-
     def insensitive_order(attribute)
-      order(build_criteria(attribute))
+      order("TRIM(LOWER(UNACCENT(#{attribute})))")
     end
   end
 end
